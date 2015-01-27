bcffa673dd28e2279f63c238922bc732c28a04f9
Michal Hantl <michal.hantl@gmail.com>
2015-01-27 19:00:49 +0100

    workaround for asset pipeline precompilation issue

--- lib/rails_web_console/engine.rb
+++ lib/rails_web_console/engine.rb
@@ -1,5 +1,12 @@
 module RailsWebConsole
   class Engine < ::Rails::Engine
     isolate_namespace RailsWebConsole
+
+
+    config.to_prepare do
+      Rails.application.config.assets.precompile += %w(
+        console.js
+      )
+    end
   end
 end
