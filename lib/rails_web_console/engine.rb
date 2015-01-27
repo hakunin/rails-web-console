e2b01c49d5f120e32c3a776415e3cbcacdaeca49
Michal Hantl <michal.hantl@gmail.com>
2015-01-27 18:34:25 +0100

    add console.js for precompilation

--- lib/rails_web_console/engine.rb
+++ lib/rails_web_console/engine.rb
@@ -1,5 +1,9 @@
 module RailsWebConsole
   class Engine < ::Rails::Engine
     isolate_namespace RailsWebConsole
+    
+    initializer "rails_web_console.assets.precompile" do |app|
+      app.config.assets.precompile += %w(console.js)
+    end 
   end
 end
